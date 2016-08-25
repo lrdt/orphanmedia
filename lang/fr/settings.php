43a037531280ced5320d4c68c2d6e5b977a0adb3
lrdt <pro@nicolas-hemard.eu>
2016-08-25 20:27:46 +0200

    French translation of settings.php

---
+++ lang/fr/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * French language file for orphanmedia plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Taggic@t-online.de
+ */
+ 
+$lang['prev_limit']         = 'Le nombre maximal d'images à prévisualiser';
+$lang['admin_orphanmedia']  = 'Détection de Médias orphelins';    
